5bc8c25ac632624b6eb900a9e25dcb18169fa21c
HELSA CHRISTABEL HARSONO <christabelhelsa@gmail.com>
2026-04-26 22:26:47 +0700

    feat: update Firebase configuration to use environment variables

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlZ6KP4BZUuv2aBLOwwGOIiUmJy6ePgn0",
-  authDomain: "chat-jagakuin.firebaseapp.com",
-  projectId: "chat-jagakuin"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
